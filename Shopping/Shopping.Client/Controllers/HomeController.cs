7fc4a3da0c908d0cc31af846e61caee3d348a660
Tharesh Sharma <tharesh.Sharma@faro.com>
2023-03-07 15:54:29 +0100

    First commit.

---
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Client.Models;
+using System.Diagnostics;
+using Shopping.Client.Data;
+
+namespace Shopping.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(ProductContext.Products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Shopping/Shopping.Client/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace Shopping.Client.Models
+{
+    public class Product
+    {
+        public string Id { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+        
+        public string Category { get; set; } = null!;
+        
+        public string Description { get; set; } = null!;
+        
+        public string ImageFile { get; set; } = null!;
+        
+        public decimal Price { get; set; }
+    }
+}
